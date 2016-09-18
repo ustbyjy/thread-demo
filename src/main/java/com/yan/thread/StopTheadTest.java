f7f30b55e12667322de38560a74f0c94b1d838f6
jingyangyan <jingyangyan@sohu-inc.com>
2016-09-18 10:26:00 +0800

    stop thread test1

--- src/main/java/com/yan/thread/StopTheadTest.java
+++ src/main/java/com/yan/thread/StopTheadTest.java
@@ -1,14 +1,14 @@
 package com.yan.thread;
 
 /**
- * stop thread
+ * stop thread: 设置标记来中断异常，安全，较为常用
  *
  * @author jingyangyan
  * @version 1.0
  * @create 2016-09-12 17:34
  */
 public class StopTheadTest extends Thread {
-    public volatile boolean exit = false;
+    private volatile boolean exit = false;
 
     @Override
     public void run() {
@@ -17,11 +17,16 @@ public class StopTheadTest extends Thread {
         }
     }
 
+    public void setExit(boolean exit) {
+        this.exit = exit;
+    }
+
     public static void main(String[] args) throws Exception {
         StopTheadTest thread = new StopTheadTest();
         thread.start();
         sleep(5000);
-        thread.exit = true;
+        thread.setExit(true);
         System.out.println("线程退出！");
     }
+
 }
